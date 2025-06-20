573eed7a36d5f018903a1af462ad71dbab7beaae
Patrick Lea <ledpup@hotmail.com>
2025-06-21 09:34:28 +1000

    Create LPBlazorMapLibrary NuGet package and deploy

--- DPBlazorMapLibrary/JsInterops/Base/JsInteropConfig.cs
+++ DPBlazorMapLibrary/JsInterops/Base/JsInteropConfig.cs
@@ -2,7 +2,7 @@
 {
     public static class JsInteropConfig
     {
-        public const string BaseJsFolder = "./_content/DPBlazorMapLibrary/";
+        public const string BaseJsFolder = "./_content/LPBlazorMapLibrary/";
         public const string MapFile = "map.js";
         public const string EventedFile = "evented.js";
         public const string IconFactoryFile = "iconFactory.js";
